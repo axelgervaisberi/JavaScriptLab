ef907cf60efe42daaa06a03a6eed5689a8ed3922
Gervais BERI <beriaxel@gmail.com>
2024-09-25 07:43:15 +0000

    Created a restaurantMenu folder and added two files to it: restaurant_menu.html and restaurant_menu.js

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,23 @@
+const breakfastMenu = ["Pancakes", "Eggs Benedict", "Oatmeal", "Frittata"];
+const maincourseMenu = ["Steak", "Pasta", "Burger", "Salmon"];
+const dessertMenu = ["Cake", "Ice Cream", "Pudding", "Fruit Salad"];
+const breakfastMenuItems = document.getElementById("breakfastMenuItems");
+const maincourseMenuItems = document.getElementById("maincourseMenuItems");
+const dessertMenuItems = document.getElementById("dessertMenuItems");
+
+const breakfastMenuItemsHTML = breakfastMenu
+  .map((item, index) => `<p>Item ${index + 1}: ${item}</p>`)
+  .join("");
+breakfastMenuItems.innerHTML = breakfastMenuItemsHTML;
+
+let maincourseItemsHTML = "";
+maincourseMenu.forEach((item, index) => {
+  maincourseItemsHTML += `<p>Item ${index + 1}: ${item}</p>`;
+});
+maincourseMenuItems.innerHTML = maincourseItemsHTML;
+
+let dessertItemHTML = "";
+for (i = 0; i < dessertMenu.length; i++) {
+  dessertItemHTML += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+dessertMenuItems.innerHTML = dessertItemHTML;
